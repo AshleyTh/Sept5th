bc3047a3e0ec449e2ef3a23c2427f43e94752bdf
AshleyTh <80226781+AshleyTh@users.noreply.github.com>
2021-10-14 19:16:44 -0400

    Add files via upload

---
+++ public/serviceworker.js
@@ -0,0 +1,12 @@
+serviceWorkerContainer.register(scriptURL, options)
+    .then(function (serviceWorkerRegistration) { ... });
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').then(function (registration) {
+        console.log('Service worker registration succeeded:', registration);
+    }, function (error) {
+        console.log('Service worker registration failed:', error);
+    });
+} else {
+    console.log('Service workers are not supported.');
+}
